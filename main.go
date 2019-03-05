c0c07be4b003be37f3a61a96f07838586c45ff09
ynigoreyes <ynigoreyes@gmail.com>
2019-03-05 14:53:34 -0600

    removed the people

--- main.go
+++ main.go
@@ -1,29 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/gorilla/mux"
 )
-
-// Person person
-type Person struct {
-	ID        string   `json:"id,omitempty"`
-	Firstname string   `json:"firstname,omitempty"`
-	Lastname  string   `json:"lastname,omitempty"`
-	Address   *Address `json:"address,omitempty"`
-}
-
-// Address address
-type Address struct {
-	City  string `json:"city,omitempty"`
-	State string `json:"state,omitempty"`
-}
-
-var people []Person
 
 func main() {
 	r := mux.NewRouter()
@@ -35,14 +18,7 @@ func main() {
 		log.Printf("Defaulting to port %s", port)
 	}
 
-	people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
-	people = append(people, Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}})
-	people = append(people, Person{ID: "3", Firstname: "Francis", Lastname: "Sunday"})
-
 	r.Handle("/", fs)
-	r.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
-		json.NewEncoder(w).Encode(people)
-	})
 
 	srv := &http.Server{
 		Handler: r,
