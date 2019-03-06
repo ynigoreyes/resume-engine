1967d71e2fbec6b6541b83d8ac62e4b860bd97ac
ynigoreyes <ynigoreyes@gmail.com>
2019-03-06 12:12:27 -0600

    refactored

--- main.go
+++ main.go
@@ -19,29 +19,7 @@ func Something(w http.ResponseWriter, r *http.Request) {
 func main() {
 	r := mux.NewRouter()
 	fs := http.FileServer(http.Dir("out"))
-
-	env := os.Getenv("env")
-	if strings.Compare(env, "prod") == 0 {
-		fmt.Println("Running in Production")
-	} else {
-		fmt.Println("Running in Development")
-		err := godotenv.Load()
-		if err != nil {
-			log.Fatal("Error loading .env file")
-		}
-	}
-
 	port := os.Getenv("PORT")
-	dbConnection := os.Getenv("db_connection")
-	storageConnection := os.Getenv("storage_connection")
-
-	if dbConnection == "" {
-		panic("Please make sure to define the db connection")
-	}
-
-	if storageConnection == "" {
-		panic("Please make sure to define the storage connection")
-	}
 
 	if port == "" {
 		port = "8080"
@@ -58,4 +36,32 @@ func main() {
 
 	log.Printf("Listening on port %s", port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func checkEnv() {
+	var dbConnection string
+	var storageConnection string
+
+	env := os.Getenv("env")
+
+	if strings.Compare(env, "prod") == 0 {
+		fmt.Println("Running in Production")
+	} else {
+		fmt.Println("Running in Development")
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+	}
+
+	dbConnection = os.Getenv("db_connection")
+	storageConnection = os.Getenv("storage_connection")
+
+	if dbConnection == "" {
+		panic("Please make sure to define the db connection")
+	}
+
+	if storageConnection == "" {
+		panic("Please make sure to define the storage connection")
+	}
 }
