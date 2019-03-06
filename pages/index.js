b4325a79bc0b09798fc86f56131c1cd706ed9a79
ynigoreyes <ynigoreyes@gmail.com>
2019-03-06 17:48:46 -0600

    adding eslint and prettier

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import Head from 'next/head'
 
 function App() {
-  return <div>Hello, From Next</div>
+  return (
+    <main>
+      <Head>
+        <title>Google Cloud</title>
+        <link
+          rel='stylesheet'
+          href='//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css'
+        />
+      </Head>
+      Hello, From Next
+    </main>
+  )
 }
 
 export default App
