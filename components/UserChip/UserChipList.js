4cf00e4e80a2f6da7abb0bacf62fe03aadb22076
ynigoreyes <ynigoreyes@gmail.com>
2019-03-08 00:14:13 -0600

    connected the public resume's

--- components/UserChip/UserChipList.js
+++ components/UserChip/UserChipList.js
@@ -1,1 +1,21 @@
-export default () => 'Hello from UserChipList'
+import React from 'react'
+import UserChip from './UserChip'
+
+function UserChipList() {
+  const chips = []
+
+  for (let i = 1; i < 5; i += 1)
+    chips.push(
+      <UserChip
+        resume={`https://storage.googleapis.com/resume-engine.appspot.com/resume_${i}.pdf`}
+        key={`https://storage.googleapis.com/resume-engine.appspot.com/resume_${i}.pdf`} // TODO: Change this to something better
+        userId={i.toString()} // Have this be a real userId later
+        desc={i.toString()} // Have this be a real user description
+        fetchComments={() => ({})}
+      />,
+    )
+
+  return <div>{chips}</div>
+}
+
+export default UserChipList
