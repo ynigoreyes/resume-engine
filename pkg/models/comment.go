7fde92ae02ef4f6fe506740f8bf86730f9348f8b
Simon W <b30bd351371c686298d32281b337e8@gmail.com>
2019-03-11 09:05:22 -0500

    Update Comment struct and add Validate method
      - Comment.CommenterID is a foreign key to User.ID
      - All *.ResumeID fields removed

--- pkg/models/comment.go
+++ pkg/models/comment.go
@@ -2,12 +2,30 @@ package models
 
 import (
 	"github.com/jinzhu/gorm"
+	"net/url"
 )
 
 // Comment defines the data necessary for representing a comment on a resume
 type Comment struct {
 	gorm.Model
-	CommenterID string `json:"commenter_id"`
-	ResumeID    string `json:"resume_id"`
+	CommenterID string `json:"commenter_id" gorm:"foreignkey:UserID"`
 	CommentBody string `json:"comment_body" gorm:"type:varchar(140)"`
+}
+
+// Validate valides a comment creation request
+func (c *Comment) Validate() url.Values {
+	// Track request body errors for response
+	errs := url.Values{}
+
+	// Check if the commenter's id empty
+	if c.CommenterID == "" {
+		errs.Add("commenter_id", "commenter_id field is required")
+	}
+
+	// Check if the comment body is empty
+	if c.CommentBody == "" {
+		errs.Add("comment_body", "comment_body field is required")
+	}
+
+	return errs
 }
